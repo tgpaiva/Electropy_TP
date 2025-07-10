bcd1d0d45816af85af07da7f7681ebef03a8de84
tgpaiva <53049604+tgpaiva@users.noreply.github.com>
2025-07-10 18:22:09 +0100

    housekeeping 10-7-2025

--- autolab_funcs.py
+++ autolab_funcs.py
@@ -31,37 +31,29 @@ def import_data(path):
     files_txt = [i for i in files if i.endswith('.txt')]
     files_txt = natsorted(files_txt)
     data = [pd.read_csv(path / dataset, delimiter = '\t', header = 0) for dataset in files_txt]
-
-    # names = [name for name in data.columns]
     name = re.split('/', str(path))[-1]
 
-    return name, data 
+    return name, data
 
 def parse_deposition_file(file_path):
     """
     import deposition data from autolab potentiostats
     returns a dataframe with time and potential columns
     """
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         headers = None  # To store the initial header
         data = []       # To store the data rows
-        
         for line in file:
-            # Strip whitespace and skip empty lines
-            line = line.strip()
+            line = line.strip() # Strip whitespace and skip empty lines
             if not line:
                 continue
-            
-            # Check if the line is a header
+
             if line.startswith("Time (s)	WE(1).Potential (V)"):
-                # If headers are already set, skip repeated header lines
-                if headers is None:
+                if headers is None:  # If headers are already set, skip repeated header lines
                     headers = line  # Store the first occurrence of the header
                 continue  # Skip any subsequent headers
-            
-            # Add non-header rows to data
-            data.append(line.split('\t'))
 
+            data.append(line.split('\t'))
     data = pd.DataFrame(data[1:], columns=['Time', 'Potential']).apply(pd.to_numeric)
 
     return data
@@ -70,11 +62,10 @@ def plot_pulsed_deposit_data(data):
     """
     Plots deposition data
     """
-    plt.plot(data['Time'], data['Potential'], color = 'black', marker = 'o', linestyle = ' ', markersize = 5) 
+    plt.plot(data['Time'], data['Potential'], color = 'black', 
+             marker = 'o', linestyle = ' ', markersize = 5)
     plt.xlabel('Time (s)')
     plt.ylabel('Potential (V)')
-    
-    return
 
 def _order_cv_data(data):
     """
@@ -92,20 +83,14 @@ def calculate_capacitance(dataset, save_results = 'n', ohmic_drop  = True, curre
     Calculate performance metrics from GCD curves 
     Current_values if none are specified the default is 0.5, 1, 2, 4, 8, 10 A/g
     """
-    # Prepare datasets
-
     df = [pd.concat([dataset[i], dataset[18 + i]], ignore_index = True) for i in range(2, 18, 3)]
     df = [pd.concat([frame['Time (s)'] - frame['Time (s)'].iloc[0], frame['WE(1).Potential (V)']], axis=1) for frame in df]
-
-   # Calculations
 
     if current_values is None:
         current_values = [0.5, 1, 2, 4, 8, 10]
 
     results = []
-
     for frame, current in zip(df, current_values):
-
         charge_time = frame['Time (s)'][frame['WE(1).Potential (V)'].idxmax()]
 
         if ohmic_drop is False:
@@ -118,7 +103,6 @@ def calculate_capacitance(dataset, save_results = 'n', ohmic_drop  = True, curre
 
         q_charge = charge_time * current / max_potential
         q_discharge = discharge_time * current / max_potential
-
         results.append({
             'Current': current,
             'Q charge (F/g)': q_charge,
@@ -162,9 +146,6 @@ def plot_gcd_curve(dataset, ylim = None ,save_plot = 'n', outname = 'GCD', label
     if save_plot == 'y':
         plt.savefig(HOME_FOLDER / f'{outname}.pdf' )
 
-    return
-
-
 def plot_cv(data, saveplot = 'n',  normalize = False, outname = 'CV', active_mass = None, colors = None ,labels = None):
     """
     Plot all CV curves at different scan rates for the same sample 
@@ -173,12 +154,10 @@ def plot_cv(data, saveplot = 'n',  normalize = False, outname = 'CV', active_mas
     cv_3rd_curve = [data[n][-number_points::] for n in range(len(data))]
 
     if normalize is True:
-
-        for n in range(len(cv_3rd_curve)):
-            cv_3rd_curve[n]['WE(1).Current (A)'] = cv_3rd_curve[n]['WE(1).Current (A)'] / active_mass
+        for n, curve in enumerate(cv_3rd_curve):
+            curve[n]['WE(1).Current (A)'] = curve[n]['WE(1).Current (A)'] / active_mass
 
     fig, ax = plt.subplots()
-
     if colors is None:
         colors = ['#0a1170','#cc9456','#7a2233','#be3b49', '#6c727a', '#6b9fe3']
     # colors = ["#1b4f72", "#b45f06", "#1d8348", "#922b21", "#5b2c6f"]
@@ -187,22 +166,20 @@ def plot_cv(data, saveplot = 'n',  normalize = False, outname = 'CV', active_mas
         labels = ['5 mV/s' , '10 mV/s', '20 mV/s', '50 mV/s', '100 mV/s' , '200 mV/s' ]
 
     for index, color in zip(range(len(cv_3rd_curve)), colors):
-        ax.plot(cv_3rd_curve[index]['Potential applied (V)'], cv_3rd_curve[index]['WE(1).Current (A)'] , color = color, linewidth = 1.8, marker = None)
+        ax.plot(cv_3rd_curve[index]['Potential applied (V)'], cv_3rd_curve[index]['WE(1).Current (A)'] , 
+                color = color, linewidth = 1.8, marker = None)
 
     ax.set_xlabel('Potential (V)', weight = 'bold')
     ax.set_ylabel('Current (A)', weight = 'bold')
 
     if normalize is True:
-         ax.set_ylabel('Current Density (A/g)', weight = 'bold')
+        ax.set_ylabel('Current Density (A/g)', weight = 'bold')
 
     ax.legend(labels, frameon = False, loc = 'lower right', ncols = 2 )
     fig.tight_layout()
 
     if saveplot == 'y':
         plt.savefig(HOME_FOLDER / f'{outname}.pdf')
-    
-    return
-
 
 def integrate_cv(data, model = 'simpson'):
     """
@@ -226,16 +203,13 @@ def integrate_cv(data, model = 'simpson'):
     elif model == 'paiva':
         # Calculate the differences in potential (ΔV)
         delta_v = np.diff(potential)
-
         # Calculate the average current values ((I_i + I_{i+1}) / 2)
         average_current = (current[:-1] + current[1:]) / 2
-
         # Calculate the area for each trapezoid (average_current * delta_v)
         areas = average_current * delta_v
-
         # Sum the areas to get the total integrated area
         charge = np.sum(areas)
-        
+
     return charge
 
 
@@ -245,51 +219,36 @@ def plot_specific_cap(path):
     """
     files = os.listdir(path)
     files = natsorted(files)
-
     data = [pd.read_excel(path + dataset) for dataset in files]
-
-    # Plot cosmetics 
-
     fig, ax = plt.subplots()
     colors = ['#000000' , '#0a1170','#cc9456','#7a2233','#be3b49', '#6c727a', '#6b9fe3','#8386bd' ]
     labels = [filename.split('.')[-2] for filename in files]   
 
-    # actually plot
-
     for index, color in zip(range(len(data))  , colors):
         ax.plot(data[index]['Current'], data[index]['Q discharge (F/g)'], color = color, linewidth = 2, marker = 'o')
-
-    # Axis decoration
 
     ax.set_xlabel('Applied Current (A)', weight = 'bold')
     ax.set_ylabel('Specific Capacitance', weight = 'bold')
     ax.legend(labels, frameon = False)
     fig.tight_layout()
 
-    return 
-
-def plot_multiple_cv(pathtofiles, CVscan_rate = '50 mV/s'):
+def plot_multiple_cv(pathtofiles, cv_scan_rate = '50 mV/s'):
     """
     Plots CV from different samples
     takes as first argument list of paths to files
     second argument is the CV scan rate, default is 50 mV/s
     """
-
     names = []
     cvdata = []
 
     for path in pathtofiles:
-
         name_out, cvdata_out = import_data(path)
         names.append(name_out)
         cvdata.append(cvdata_out)
 
-    CV_data_dic = [_order_cv_data(data) for data in cvdata]
-
-    fig, ax = plt.subplots() 
-
+    cv_data_dic = [_order_cv_data(data) for data in cvdata]
+    fig, ax = plt.subplots()
     # colors = ['#0a1170','#cc9456','#7a2233','#be3b49', '#6c727a', '#6b9fe3']
-
     colors = [
     "#8B0000",  # Dark Red
     "#00008B",  # Dark Blue
@@ -300,18 +259,10 @@ def plot_multiple_cv(pathtofiles, CVscan_rate = '50 mV/s'):
     "#2F4F4F",  # Dark Slate Gray
     "#8B4513"   # Saddle Brown
     ]
-    
-    for index, data in enumerate(CV_data_dic):
 
-        ax.plot(data[CVscan_rate]['Potential applied (V)'], data[CVscan_rate]['WE(1).Current (A)'], label = names[index], color = colors[index])
+    for index, data in enumerate(cv_data_dic):
+        ax.plot(data[cv_scan_rate]['Potential applied (V)'], data[cv_scan_rate]['WE(1).Current (A)'], 
+                label = names[index], color = colors[index])
         ax.set_xlabel('Potential (V)', weight = 'bold')
         ax.set_ylabel('Current (A)', weight = 'bold')
         ax.legend(frameon = False)
-
-    return 
-
-
-
-
-
-
